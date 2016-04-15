9dca5e6bfcdbd17982c0a301bdea3552c3ca35eb
Milton Brandes <miltonbrandess@hotmail.com>
2016-04-15 15:06:20 -0300

    Subidos solo los .c

---
+++ consola.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+int main(int argc, char *argv[]) {
+	printf("Arranca Consola\n"); /*aca deberia conectarme directamente con el nucleo cuando se inicia*/
+
+	return 0;
+}
---
+++ procesoCpu.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+
+
