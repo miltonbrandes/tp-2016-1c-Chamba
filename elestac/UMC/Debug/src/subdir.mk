fc2f66cd8660e94698ea5b693815e41f7e3753c9
mvlaicevich <mvlaicevich@gmail.com>
2016-04-21 17:58:20 -0300

    Elimino archivos binarios

--- elestac/UMC/Debug/src/subdir.mk
+++ elestac/UMC/Debug/src/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/Escritorio/workspace/sockets" -I"/home/utnso/Escritorio/workspace/commons" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I"/home/utnso/workspace/sockets" -I"/home/utnso/workspace/commons" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
