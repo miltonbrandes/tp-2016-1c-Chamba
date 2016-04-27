4ecf5f95bddc65bdfb3c60ac868a22fa1f9a211d
milton brandes <miltonbrandess@hotmail.com>
2016-04-26 22:26:39 -0300

    Fix de las estructuras de header y payload a usar, ya se las puede usar

--- elestac/sockets/sockets/EscrituraLectura.h
+++ elestac/sockets/sockets/EscrituraLectura.h
@@ -7,41 +7,40 @@
 
 #ifndef LIBRERIAS_SOCKETSESCRITURALECTURA_H_
 #define LIBRERIAS_SOCKETSESCRITURALECTURA_H_
-#include <sys/socket.h>
-#include <stdint.h>
-//int recibirDatos(int socket, char** datos, char* tamañoOcupa);
-
 enum  { CONSOLA=1, CPU=2, NUCLEO=3, SWAP=4, UMC=5 } quien_conecto;
-enum  {ENVIAR_MENSAJE_CONSOLA=1} operaciones_consola;
-enum  {ENVIAR_MENSAJE_NUCLEO=1} operaciones_nucleo;
-enum  {ENVIAR_MENSAJE_CPU=1} operaciones_cpu;
-enum  {ENVIAR_MENSAJE_UMC=1} operaciones_umc;
-enum  {ENVIAR_MENSAJE_SWAP=1} operaciones_swap;
+enum{ENVIAR_MENSAJE_CONSOLA = 1}operaciones_consola;
+enum{ENVIAR_MENSAJE_NUCLEO= 1}operaciones_nucleo;
+enum{ENVIAR_MENSAJE_CPU = 1}operaciones_cpu;
+enum{ENVIAR_MENSAJE_UMC = 1}operaciones_umc;
+enum{ENVIAR_MENSAJE_SWAP= 1}operaciones_swap;
 
-typedef struct {
+typedef struct{
 	char* mensaje;
-} t_paquete_mensaje;
 
-typedef struct {
-	int quienConecto;
+}t_paquete_mensaje;
+
+typedef struct{
+	int quien_conecto;
 	int operacion;
 	int longitudPayload;
-} t_header;
+}t_header;
 
-typedef struct {
+typedef struct{
 	void *datos;
-} t_payload;
+}t_payload;
 
-typedef struct {
+typedef struct{
 	t_header* header;
 	t_payload* payload;
-} t_paquete;
+}t_paquete;
 
+#include <sys/socket.h>
+#include <stdint.h>
 
-int leer(int socket, t_paquete* paquete, int longitud);
-int escribir(int socket, t_paquete* paquete, int longitud);
+//int recibirDatos(int socket, char** datos, char* tamañoOcupa);
+int leer(int socket, char* buffer, int longitud);
+int escribir(int socket, char* buffer, int longitud);
+
 
 int finalizarConexion(int socket);
-
-
 #endif /* LIBRERIAS_SOCKETSESCRITURALECTURA_H_ */
