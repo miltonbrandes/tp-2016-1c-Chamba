30cc25513ea1adb8ebbe2829842d37e361b41604
JulianEzequiel <julian.e.pico@gmail.com>
2016-04-29 20:24:13 -0300

    Otro cambio en config

--- ConfigAutomatico.sh
+++ ConfigAutomatico.sh
@@ -1,11 +1,22 @@
-cd /home/utnso/workspace/tp-2016-1c-Chamba/elestac/sockets/Debug/
+#Libraries
+cd /home/utnso/tp-2016-1c-Chamba/
+git clone https://github.com/sisoputnfrba/so-commons-library.git
+
+echo "DESCARGA DE LAS COMMONS COMPLETA"
+echo "PROCEDEMOS A INSTALARLAS"
+cd so-commons-library
 sudo make clean
 sudo make all
 sudo make install
 
-#Libraries
-cd /home/utnso/workspace/tp-2016-1c-Chamba/elestac/
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/utnso/workspace/tp-2016-1c-Chamba/elestac/sockets/Debug/
+cd /home/utnso/tp-2016-1c-Chamba/elestac/
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/utnso/tp-2016-1c-Chamba/so-commons-library/src/build/
+
+cd /home/utnso/tp-2016-1c-Chamba/elestac/sockets/Debug/
+sudo make clean
+sudo make all
+sudo make install
+
 
 cd /home/utnso/workspace/tp-2016-1c-Chamba/
 git clone https://github.com/sisoputnfrba/ansisop-parser.git
@@ -17,18 +28,8 @@ sudo make clean
 sudo make all
 sudo make install
 
-cd /home/utnso/workspace/tp-2016-1c-Chamba/elestac/
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/utnso/workspace/tp-2016-1c-Chamba/ansisop-parser/parser/build/
+cd /home/utnso/tp-2016-1c-Chamba/elestac/
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/utnso/tp-2016-1c-Chamba/ansisop-parser/parser/build/
 
-cd /home/utnso/workspace/tp-2016-1c-Chamba/
-git clone https://github.com/sisoputnfrba/so-commons-library.git
-
-echo "DESCARGA DE LAS COMMONS COMPLETA"
-echo "PROCEDEMOS A INSTALARLAS"
-cd so-commons-library
-sudo make clean
-sudo make all
-sudo make install
-
-cd /home/utnso/workspace/tp-2016-1c-Chamba/elestac/
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/utnso/workspace/tp-2016-1c-Chamba/so-commons-library/src/build/
+cd /home/utnso/tp-2016-1c-Chamba/elestac/
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/utnso/tp-2016-1c-Chamba/elestac/sockets/Debug/
