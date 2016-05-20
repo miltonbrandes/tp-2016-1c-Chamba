f68b1c0ecafed62a29a235109968c6c9551158e0
mvlaicevich <mvlaicevich@gmail.com>
2016-05-20 13:47:55 -0300

    Avanzando con UMC

--- elestac/UMC/src/UMC.c
+++ elestac/UMC/src/UMC.c
@@ -68,23 +68,8 @@ typedef struct {
 	int numCpu;
 	int socket;
 	bool enUso;
+	pthread_t hiloCpu;
 } t_cpu;
-
-//sem_t semEsperaCPU;
-
-//struct para conexiones
-
-typedef struct Conexiones {
-	int socket_escucha;					// Socket de conexiones entrantes
-	struct sockaddr_in direccion;
-	// Datos de la direccion del servidor
-	socklen_t tamanio_direccion;		// Tamaño de la direccion
-	//t_cpu CPUS[miContexto.cantHilosCpus];						// Sockets de conexiones ACEPTADAS
-	//hago el vector de arriba de forma dinamica
-	t_cpu *CPUS; //apunta al primer elemento del vector dinamico
-} t_conexiones;
-
-//TODO: falta que el umc reciba cuando hay un programa nuevo de cpu!!!!
 
 #define MAX_BUFFER_SIZE 4096
 
@@ -100,20 +85,15 @@ t_log* ptrLog;
 int puertoTCPRecibirConexionesCPU;
 int puertoTCPRecibirConexionesNucleo;
 int puertoReceptorSwap;
-int marcos, marcosSize, marcoXProc, entradasTLB, retardo, tlbHabilitada,
-		hilosCpus;
+int marcos, marcosSize, marcoXProc, entradasTLB, retardo, tlbHabilitada;
 
 char *ipSwap;
-//int *listaCpus;
 int i = 0;
 
 //Variables frames, tlb
 t_list * framesOcupados;
 t_list * framesVacios;
 
-pthread_t threadSwap;
-pthread_t threadNucleo;
-pthread_t threadCPU;
 
 //Metodos para Iniciar valores de la UMC
 int crearLog() {
@@ -131,48 +111,42 @@ int iniciarUMC(t_config* config) {
 	if (config_has_property(config, "MARCOS")) {
 		marcos = config_get_int_value(config, "MARCOS");
 	} else {
-		log_info(ptrLog,
-				"El archivo de configuracion no contiene la clave MARCOS");
+		log_info(ptrLog, "El archivo de configuracion no contiene la clave MARCOS");
 		return 0;
 	}
 
 	if (config_has_property(config, "MARCOS_SIZE")) {
 		marcosSize = config_get_int_value(config, "MARCOS_SIZE");
 	} else {
-		log_info(ptrLog,
-				"El archivo de configuracion no contiene la clave MARCOS_SIZE");
+		log_info(ptrLog, "El archivo de configuracion no contiene la clave MARCOS_SIZE");
 		return 0;
 	}
 
 	if (config_has_property(config, "MARCO_X_PROC")) {
 		marcoXProc = config_get_int_value(config, "MARCO_X_PROC");
 	} else {
-		log_info(ptrLog,
-				"El archivo de configuracion no contiene la clave MARCO_X_PROC");
+		log_info(ptrLog, "El archivo de configuracion no contiene la clave MARCO_X_PROC");
 		return 0;
 	}
 
 	if (config_has_property(config, "ENTRADAS_TLB")) {
 		entradasTLB = config_get_int_value(config, "ENTRADAS_TLB");
 	} else {
-		log_info(ptrLog,
-				"El archivo de configuracion no contiene la clave ENTRADAS_TLB");
+		log_info(ptrLog, "El archivo de configuracion no contiene la clave ENTRADAS_TLB");
 		return 0;
 	}
 
 	if (config_has_property(config, "RETARDO")) {
 		retardo = config_get_int_value(config, "RETARDO");
 	} else {
-		log_info(ptrLog,
-				"El archivo de configuracion no contiene la clave RETARDO");
+		log_info(ptrLog, "El archivo de configuracion no contiene la clave RETARDO");
 		return 0;
 	}
 
 	if (config_has_property(config, "TLB_HABILITADA")) {
 		tlbHabilitada = config_get_int_value(config, "TLB_HABILITADA");
 	} else {
-		log_info(ptrLog,
-				"El archivo de configuracion no contiene la clave TLB_HABILITADA");
+		log_info(ptrLog, "El archivo de configuracion no contiene la clave TLB_HABILITADA");
 		return 0;
 	}
 
@@ -185,36 +159,30 @@ int cargarValoresDeConfig() {
 
 	if (config) {
 		if (config_has_property(config, "PUERTO_CPU")) {
-			puertoTCPRecibirConexionesCPU = config_get_int_value(config,
-					"PUERTO_CPU");
+			puertoTCPRecibirConexionesCPU = config_get_int_value(config, "PUERTO_CPU");
 		} else {
-			log_info(ptrLog,
-					"El archivo de configuracion no contiene la clave PUERTO_CPU");
+			log_info(ptrLog, "El archivo de configuracion no contiene la clave PUERTO_CPU");
 			return 0;
 		}
 
 		if (config_has_property(config, "PUERTO_NUCLE0")) {
-			puertoTCPRecibirConexionesNucleo = config_get_int_value(config,
-					"PUERTO_NUCLE0");
+			puertoTCPRecibirConexionesNucleo = config_get_int_value(config, "PUERTO_NUCLE0");
 		} else {
-			log_info(ptrLog,
-					"El archivo de configuracion no contiene la clave PUERTO_NUCLE0");
+			log_info(ptrLog, "El archivo de configuracion no contiene la clave PUERTO_NUCLE0");
 			return 0;
 		}
 
 		if (config_has_property(config, "IP_SWAP")) {
 			ipSwap = config_get_string_value(config, "IP_SWAP");
 		} else {
-			log_info(ptrLog,
-					"El archivo de configuracion no contiene la clave IP_SWAP");
+			log_info(ptrLog, "El archivo de configuracion no contiene la clave IP_SWAP");
 			return 0;
 		}
 
 		if (config_has_property(config, "PUERTO_SWAP")) {
 			puertoReceptorSwap = config_get_int_value(config, "PUERTO_SWAP");
 		} else {
-			log_info(ptrLog,
-					"El archivo de configuracion no contiene la clave PUERTO_SWAP");
+			log_info(ptrLog, "El archivo de configuracion no contiene la clave PUERTO_SWAP");
 			return 0;
 		}
 
@@ -241,8 +209,7 @@ void datosEnSocketReceptorNucleoCPU(int socketNuevaConexion) {
 	int bytesRecibidos = strlen(buffer);
 
 	if (bytesRecibidos < 0) {
-		log_info(ptrLog,
-				"Ocurrio un error al recibir datos en un Socket Nucleo o CPU");
+		log_info(ptrLog, "Ocurrio un error al recibir datos en un Socket Nucleo o CPU");
 	} else if (bytesRecibidos == 0) {
 		log_info(ptrLog, "No se recibieron datos en el Socket Nucleo o CPU");
 	} else {
@@ -265,8 +232,7 @@ int datosEnSocketSwap() {
 		log_info(ptrLog, "Ocurrio un error al recibir datos de Swap");
 		return -1;
 	} else if (bytesRecibidos == 0) {
-		log_info(ptrLog,
-				"No se recibieron datos de Swap. Se cierra la conexion");
+		log_info(ptrLog, "No se recibieron datos de Swap. Se cierra la conexion");
 		finalizarConexion(socketSwap);
 		return -1;
 	} else {
@@ -298,136 +264,46 @@ int obtenerSocketMaximoInicial() {
 	return socketMaximoInicial;
 }
 
-void *escuchar(struct Conexiones* conexion) {
-	int i = 1;
-	//semEsperaCPU.__align = 0; // inicializa semaforo
 
-	//conexion para el comando cpu
-	conexion->CPUS[0].socket = accept(conexion->socket_escucha,
-			(struct sockaddr *) &conexion->direccion,
-			&conexion->tamanio_direccion);
-
-	while (i <= hilosCpus) //hasta q recorra todos los hilos de cpus habilitados
-	{
-		//guarda las nuevas conexiones para acceder a ellas desde cualquier parte del codigo
-		conexion->CPUS[i].socket = accept(conexion->socket_escucha,
-				(struct sockaddr *) &conexion->direccion,
-				&conexion->tamanio_direccion);
-		if (conexion->CPUS[i].socket == -1) {
-			perror("ACCEPT");	//control error
-		}
-		conexion->CPUS[i].numCpu = i;
-		conexion->CPUS[i].enUso = false;
-		//ACA ME FIJO QUE OPERACION ES
-		//	sem_post(&semEsperaCPU); //avisa que hay 1 CPU disponible
-		//puts("NUEVO HILO ESCUCHA!\n");
-		//log_info(logger, "CPU %d conectado", i);
-		i++;
+void AceptarConexionCpu(t_list *listaCpus){
+	int socketCpu = AceptarConexionCliente(socketReceptorCPU);
+	if (socketCpu < 0) {
+		log_info(ptrLog, "Ocurrio un error al intentar aceptar una conexion de CPU");
+	} else {
+		log_info(ptrLog, "Nueva conexion de CPU");
+		t_cpu* cpu;
+		int num = list_size(listaCpus);
+		cpu->socket = socketCpu;
+		cpu->numCpu = num + 1;
+		cpu->enUso = false;
+		list_add(listaCpus, cpu);
 	}
 
-	return NULL;
-}
-
 void manejarConexionesRecibidas() {
-	//int handshakeNucleo = 0;
-	fd_set sockets, tempSockets;
-	//int resultadoSelect;
-
-	int socketMaximo = obtenerSocketMaximoInicial();
-	int socketCPUPosta = -121211;
-
-	FD_SET(socketReceptorNucleo, &sockets);
-	FD_SET(socketReceptorCPU, &sockets);
-	FD_SET(socketSwap, &sockets);
-
+	int socketNucleo = AceptarConexionCliente(socketReceptorNucleo);
+	if (socketNucleo < 0) {
+		log_info(ptrLog, "Ocurrio un error al intentar aceptar una conexion de Nucleo");
+	} else {
+		log_info(ptrLog, "Se conecto Nucleo");
+	}
+	listaCpus = list_create();
 	while (1) {
-		FD_ZERO(&tempSockets);
-		log_info(ptrLog, "Esperando conexiones");
-		tempSockets = sockets;
-		int resultadoSelect = select(socketMaximo + 1, &tempSockets, NULL, NULL,
-				NULL);
-		if (resultadoSelect == 0) {
-			log_info(ptrLog, "Time out. Volviendo a esperar conexiones");
-		} else if (resultadoSelect < 0) {
-			log_info(ptrLog, "Ocurrio un error");
-		} else {
-
-			if (FD_ISSET(socketReceptorNucleo, &tempSockets)) {
-
-				int nuevoSocketConexion = AceptarConexionCliente(
-						socketReceptorNucleo);
-				if (nuevoSocketConexion < 0) {
-					log_info(ptrLog,
-							"Ocurrio un error al intentar aceptar una conexion de CPU o Nucleo");
-				} else {
-					FD_SET(nuevoSocketConexion, &sockets);
-					FD_SET(nuevoSocketConexion, &tempSockets);
-					socketMaximo =
-							(socketMaximo < nuevoSocketConexion) ?
-									nuevoSocketConexion : socketMaximo;
-				}
-				FD_CLR(socketReceptorNucleo, &tempSockets);
-
-			} else if (FD_ISSET(socketReceptorCPU, &tempSockets)) {
-
-				int nuevoSocketConexion = AceptarConexionCliente(
-						socketReceptorCPU);
-				if (nuevoSocketConexion < 0) {
-					log_info(ptrLog,
-							"Ocurrio un error al intentar aceptar una conexion de CPU");
-				} else {
-					FD_SET(nuevoSocketConexion, &sockets);
-					FD_SET(nuevoSocketConexion, &tempSockets);
-					socketMaximo =
-							(socketMaximo < nuevoSocketConexion) ?
-									nuevoSocketConexion : socketMaximo;
-				}
-				//aca deberia ir un mensaje hacia swap diciendo que cpu me esta pidiendo espacio, = que con el nucleo usar una lista para cpus!!!
-
-				//agrego las cpus a una nueva lista
-				//			conexiones.CPUS[i].socket = nuevoSocketConexion;
-				//			conexiones.CPUS[i].numCpu = i++;
-				//			conexiones.CPUS[i].enUso = false;
-				i++;
-				socketCPUPosta = nuevoSocketConexion;
-
-				FD_CLR(socketReceptorCPU, &tempSockets);
-
-			} else if (FD_ISSET(socketSwap, &tempSockets)) {
-
-				//Ver que hacer aca, no se acepta conexion, se recibe algo de Swap.
-				int returnDeSwap = datosEnSocketSwap();
-				if (returnDeSwap == -1) {
-					//Aca matamos Socket SWAP
-					FD_CLR(socketSwap, &sockets);
-				}
-
-			} else {
-
-				//alojo memoria dinamicamente en tiempo de ejecucion
-				//	conexiones.CPUS = (t_cpu*) malloc(sizeof(t_cpu) * ((hilosCpus) + 1));
-				//		if (conexiones.CPUS == NULL)
-				puts("ERROR MALLOC 1");
-				pthread_t hilo_conexiones;
-				//		if (pthread_create(&hilo_conexiones, NULL, (void*) escuchar, &conexiones) < 0)
-				//				perror("Error HILO ESCUCHAS!");
-				//			conexiones.CPUS[0].enUso = true;
-
-				puts("UMC ESPERANDO CONEXIONES....\n\n\n");
-				//			sem_wait(&semEsperaCPU); //semaforo espera conexiones
-
+		log_info(ptrLog, "Esperando conexiones CPU");
+		pthread_t hiloCpu;
+		pthread_create(&hiloCpu, NULL, (void*) AceptarConexionCpu, &listaCpus);
+		t_cpu* cpu = list_take_and_remove(listaCpus, list_size(listaCpus));
+		cpu->hiloCpu = hiloCpu;
+		list_add(listaCpus, cpu);
+				//TODO hasta aca modifique recibir conexiones
 				//Ver que hacer aca, se esta recibiendo algo de un socket en particular
 				//recibirDatos(&tempSockets, &sockets, socketMaximo);
-				char* buffer;
+		/*		char* buffer;
 				uint32_t id;
 				uint32_t operacion;
 				int bytesRecibidos;
 				int socketFor;
-				for (socketFor = 0; socketFor < (socketMaximo + 1);
-						socketFor++) {
-					if (FD_ISSET(socketFor, &tempSockets)) {
 						buffer = recibirDatos(socketSwap, &operacion, &id);
-						int bytesRecibidos = strlen(buffer);
+						bytesRecibidos = strlen(buffer);
 						if (bytesRecibidos > 0) {
 							if(strcmp("ERROR", buffer) == 0) {
 
@@ -441,25 +317,14 @@ void manejarConexionesRecibidas() {
 							//Aca estamos matando el Socket que esta fallando
 							//Ver que hay que hacer porque puede venir de CPU o de Nucleo
 							finalizarConexion(socketFor);
-							FD_CLR(socketFor, &tempSockets);
-							FD_CLR(socketFor, &sockets);
-							log_info(ptrLog,
-									"No se recibio ningun byte de un socket que solicito conexion.");
+							log_info(ptrLog, "No se recibio ningun byte de un socket que solicito conexion.");
 						} else {
 							finalizarConexion(socketFor);
-							FD_CLR(socketFor, &sockets);
-							log_info(ptrLog,
-									"Ocurrio un error al recibir los bytes de un socket");
+							log_info(ptrLog, "Ocurrio un error al recibir los bytes de un socket");
 						}
-					}
-				}
-				if (socketCPUPosta > 0) {
-					//	enviarMensajeASwap("Se abrio un nuevo programa por favor reservame memoria");
-				}
-			}
+						*/
 		}
 	}
-}
 
 char * reservarMemoria(int cantidadMarcos, int tamanioMarco) {
 	// calloc me la llena de \0
@@ -475,25 +340,24 @@ int main() {
 			return -1;
 		}
 		log_info(ptrLog, "Se conecto con Swap");
-		socketReceptorNucleo = AbrirSocketServidor(
-				puertoTCPRecibirConexionesNucleo);
+		socketReceptorNucleo = AbrirSocketServidor(puertoTCPRecibirConexionesNucleo);
 		if (socketReceptorNucleo < 0) {
-			log_info(ptrLog,
-					"No se pudo abrir el Socket Servidor Nucleo de UMC");
+			log_info(ptrLog, "No se pudo abrir el Socket Servidor Nucleo de UMC");
 			return -1;
 		}
 		log_info(ptrLog, "Se abrio el socket Servidor Nucleo de UMC");
 		socketReceptorCPU = AbrirSocketServidor(puertoTCPRecibirConexionesCPU);
 		if (socketReceptorCPU < 0) {
-			log_info(ptrLog,
-					"No se pudo abrir el Socket Servidor Nucleo de CPU");
+			log_info(ptrLog, "No se pudo abrir el Socket Servidor Nucleo de CPU");
 			return -1;
 		}
 		log_info(ptrLog, "Se abrio el socket Servidor Nucleo de CPU");
+		enviarMensajeASwap("Abrimos bien, soy la umc");
 		manejarConexionesRecibidas();
 	} else {
 		log_info(ptrLog, "La UMC no pudo inicializarse correctamente");
 		return -1;
 	}
+	log_info(ptrLog, "Proceso UMC finalizado");
 	return EXIT_SUCCESS;
 }
