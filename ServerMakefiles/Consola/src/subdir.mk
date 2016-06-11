a2108a57e72b7f29f0371eb95eeed8665e84335e
JulianEzequiel <julian.e.pico@gmail.com>
2016-06-11 02:22:46 -0300

    Porfavor solucionate

--- ServerMakefiles/Consola/src/subdir.mk
+++ ServerMakefiles/Consola/src/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/tp-2016-1c-Chamba/elestac/sockets" -I"/home/utnso/tp-2016-1c-Chamba/so-commons-library" -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -Isockets -Icommons -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
