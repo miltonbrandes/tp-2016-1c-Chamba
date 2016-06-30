d3ac85ecb205de8b496c90b227799f521f815619
milton brandes <miltonbrandess@hotmail.com>
2016-06-30 10:14:27 -0300

    Mutex swap

--- elestac/UMC/src/UMC.c
+++ elestac/UMC/src/UMC.c
@@ -110,7 +110,7 @@ int main() {
 ////FUNCIONES UMC////
 
 char * enviarYRecibirMensajeSwap(t_buffer_tamanio * bufferTamanio, uint32_t operacion) {
-//	pthread_mutex_lock(&comunicacionConSwap);
+	pthread_mutex_lock(&comunicacionConSwap);
 	char * mensajeDeSwap;
 
 	int bytesEnviados = enviarDatos(socketSwap, bufferTamanio->buffer,
@@ -122,7 +122,7 @@ char * enviarYRecibirMensajeSwap(t_buffer_tamanio * bufferTamanio, uint32_t oper
 		uint32_t operacion, id;
 		mensajeDeSwap = recibirDatos(socketSwap, &operacion, &id);
 	}
-//	pthread_mutex_unlock(&comunicacionConSwap);
+	pthread_mutex_unlock(&comunicacionConSwap);
 	return mensajeDeSwap;
 }
 
